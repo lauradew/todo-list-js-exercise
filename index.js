b227880e8832bcc629babdfe23efff719767a950
Laura De Witt <lauramdewitt@gmail.com>
2018-02-13 01:50:58 +0000

    updated using methods .this

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    //print state of the task in a nice readable way
+    logState: function() {
+      console.log(`${this.title} has ${this.complete ? " " : " not "}been completed`);
+    },
+    //mark task as completed using methods
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -30,8 +28,6 @@ const task1 = newTask("Clean cat litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
-
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
